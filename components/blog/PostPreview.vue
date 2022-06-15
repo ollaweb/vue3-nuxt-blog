8fb1f3ce7f6a3bf3b0cc61532e27bab61288bf95
Olga L <olla.bradford@gmail.com>
2022-06-15 12:56:05 +0300

    Added posts preview on the home page

---
+++ components/blog/PostPreview.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="post-preview">
+    <div class="post-image">
+      <img :src="post.img" :alt="post.title">
+    </div>
+    <div class="post-content">
+      <h3 class="title">{{post.title}}</h3>
+      <p>{{post.descr}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .post-preview {
+    padding: 16px;
+    max-width: 33%;
+    text-align: center;
+  }
+  .post-image {
+    margin-bottom: 20px;
+    width: 100%;
+    height: 250px;
+    & img {
+      margin-bottom: 16px;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+  .post-content {
+    & p {
+      color: #999999;
+    }
+  }
+</style>
---
+++ components/blog/PostsList.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="post-list">
+    <div class="container">
+      <div class="posts__wrapper">
+        <PostPreview
+          v-for="post in posts"
+          :key="post.id"
+          :post="post"/>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/blog/PostPreview.vue'
+export default {
+  components: {
+    PostPreview
+  },
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .posts__wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+</style>
