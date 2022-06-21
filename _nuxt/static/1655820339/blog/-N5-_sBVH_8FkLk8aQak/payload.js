ce8a3956ae42c6f26946fc24aa2674ab42a51fa7
Olga L <olla.bradford@gmail.com>
2022-06-21 17:12:11 +0300

    Added dist folder

--- _nuxt/static/1655820339/blog/-N5-_sBVH_8FkLk8aQak/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/blog/-N5-_sBVH_8FkLk8aQak", (function(a){return {data:[{post:{content:"It's summer time.",descr:"Field flowers",id:a,img:"\u002Fimg\u002Fposts\u002F4.jpg",title:"Summer"},comments:[{id:"-N5-ddCfK0KUYcojEwNf",name:"Alexa",postId:a,published:true,text:"My favorite flowers"}]}],fetch:{},mutations:[]}}("-N5-_sBVH_8FkLk8aQak")));
